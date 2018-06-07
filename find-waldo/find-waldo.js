c6b523e1d313bf8aba7a4bd9035b53487b913102
byeong.kim0430@gmail.com <byeong.kim0430@gmail.com>
2018-06-07 18:26:27 +0000

    Print the index of Waldo when found

--- find-waldo/find-waldo.js
+++ find-waldo/find-waldo.js
@@ -1,14 +1,14 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  arr.forEach(function (item, index) {
-    if (item === "Waldo") {
-      found(index);
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === "Waldo") {
+      found(i);   // execute callback
     }
-  })
+  }
 }
 
 function actionWhenFound(index) {
-  console.log("Found him at " + index);
+  console.log("Found Waldo at index " + index + "!");
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
